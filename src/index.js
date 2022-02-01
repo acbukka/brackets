8ef14593d4723fd0242367b12a2deb01217c9a14
acbukka <thesweylis@gmail.com>
2022-02-01 14:26:19 +0700

    LIL

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig_orig) {
+  // create the stack:
+  let stack = [];
+  // make a copy of variable for not to changing this variable:
+  let bracketsConfig = JSON.parse(JSON.stringify(bracketsConfig_orig));
+  // reverse config to generate pairs:
+  let reverseConfig = bracketsConfig.map((item) => {
+    return item.reverse();
+  });
+  // lets convert braketsConfig to object:
+  let objConfig = {};
+  reverseConfig.forEach(function (element) {
+    objConfig[element[0]] = element[1];
+  });
+  console.log(objConfig);
+  // lets get object values and keys from objConfig to compare
+  let objectKeys = [];
+  for (let key in objConfig) {
+    objectKeys.push(key);
+  }
+  let openBrackets = [];
+  for (var i in objConfig) {
+    if (objConfig.hasOwnProperty(i)) {
+      openBrackets.push(objConfig[i]);
+    }
+  }
+  console.log(openBrackets);
+  // lets check if every part of string have the pair
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (stack[stack.length - 1] === currentSymbol && objectKeys.includes(stack[stack.length - 1])) {
+      stack.pop();
+    } else if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (objConfig[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    console.log(stack);
+  }
+  return stack.length === 0;
 }
